d15cd850a086869493682a392211bbde1d97c73f
AKajander <43338390+AKajander@users.noreply.github.com>
2018-11-11 19:34:41 +0200

    Add files via upload
    #Add do-while loop
    #Add function for adding new translations

--- Dictionary.java
+++ Dictionary.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 public class Dictionary {
 	public static void main(String args[]) {
 		
+		String search;
+		String result;
+		String originalWord;
+		String translation;
 		HashMap<String, String> words = new HashMap<>();
 	
 		words.put("kissa", "cat");
@@ -15,15 +19,28 @@ public class Dictionary {
 		Scanner reader = new Scanner(System.in);
 		
 		System.out.println("Minkä sanan käännöksen haluat tietää?");
-		String search = reader.nextLine();		
-		String result = words.get(search);
+		search = reader.nextLine();		
+		result = words.get(search);
 		
-		if (words.get(search) == null) {
-			System.out.println("Sanan \"" + search + "\" käännös tuntematon!");
-		} else if (search == null) {
-			System.out.println("Ohjelma lopetetaan, kiitos käynnistä");
-		} else {		
+		do {
+			if (words.get(search) == null) {
+				System.out.println("Sanan \"" + search + "\" käännös tuntematon!");
+			}
+			
 			System.out.println("Sanan \"" + search + "\" käännös on \"" + result + "\"");
-		}
+		} while (search == "lopeta");
+		
+		System.out.println("Ohjelma lopetetaan, kiitos käynnistä!");
+		
+		do {
+			System.out.println("Sana alkukielellä?");
+			originalWord = reader.nextLine();
+			System.out.println("Sana käännettynä?");
+			translation = reader.nextLine();
+		
+			words.put(originalWord, translation);
+		} while (originalWord == "lopeta");
+		
+		System.out.println("Ohjelma lopetetaan, kiitos käynnistä!");
 	}
 }
